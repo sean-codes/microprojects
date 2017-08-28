356b652528d3ac9481e623cc4db1047468c1f05d
sean-codes <sean_codes@outlook.com>
2017-08-28 00:20:50 -0500

    add on error to prevent sass crashing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
+const gutil = require('gulp-util')
 const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('watch', function(){
    gulp.watch('./projects/**/*.scss', ['sass'])
@@ -8,7 +9,7 @@ gulp.task('watch', function(){
 
 gulp.task('sass', function(){
    gulp.src('./projects/**/*.scss')
-      .pipe(sass())
+      .pipe(sass()).on('error', gutil.log)
       .pipe(autoprefixer())
       .pipe(gulp.dest('./projects'))
 })
