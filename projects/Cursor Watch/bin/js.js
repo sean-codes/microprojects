73c7af01661b638ed255fdfd9f808a088ade6ae4
sean-codes <sean_codes@outlook.com>
2018-01-08 22:46:08 -0600

    update safari support / mobile

--- projects/Cursor Watch/bin/js.js
+++ projects/Cursor Watch/bin/js.js
@@ -27,108 +27,65 @@ if (!window.frameElement) {
 }
 
 var CursorWatch = function () {
-   function CursorWatch(input) {
+   function CursorWatch(input, callback) {
       _classCallCheck(this, CursorWatch);
 
       this.input = input;
+      this.callback = callback;
       this.fake = document.createElement('div');
-      this.data = { position: 0, x: 0, y: 0 };
-      this.oldPosition = 0;
+      this.data = { position: 0, lastPosition: -1, x: 0, y: 0 };
       this.listeners = [];
       this.listen();
    }
 
    _createClass(CursorWatch, [{
-      key: "on",
-      value: function on(type, callback) {
-         this.listeners.push({ type: type, callback: callback });
-      }
-   }, {
-      key: "fire",
-      value: function fire(type) {
-         var _iteratorNormalCompletion = true;
-         var _didIteratorError = false;
-         var _iteratorError = undefined;
-
-         try {
-            for (var _iterator = this.listeners[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-               var listener = _step.value;
-
-               if (listener.type == type) {
-                  listener.callback(this.data);
-               }
-            }
-         } catch (err) {
-            _didIteratorError = true;
-            _iteratorError = err;
-         } finally {
-            try {
-               if (!_iteratorNormalCompletion && _iterator.return) {
-                  _iterator.return();
-               }
-            } finally {
-               if (_didIteratorError) {
-                  throw _iteratorError;
-               }
-            }
-         }
-      }
-   }, {
       key: "listen",
       value: function listen() {
          var _this = this;
 
          this.interval = setInterval(function () {
-            _this.updateCursorPosition();
-            if (_this.oldPosition != _this.data.position) {
-               _this.change();
-            }
-         }, 1000 / 30);
-
+            return _this.updateCursorPosition();
+         }, 1000 / 15);
          this.input.addEventListener('scroll', function () {
-            _this.change();
+            return _this.change();
          });
-      }
-   }, {
-      key: "change",
-      value: function change() {
-         this.updateCursorPosition();
-         this.updateFake();
-         this.updateCursorXYPosition();
-         this.fire('change');
       }
    }, {
       key: "updateCursorPosition",
       value: function updateCursorPosition() {
          this.data.position = this.input.selectionStart;
+         if (this.data.lastPosition != this.data.position) this.change();
+         this.data.lastPosition = this.data.position;
+      }
+   }, {
+      key: "change",
+      value: function change() {
+         this.updateFake();
+         this.updateCursorXYPosition();
+         this.callback();
       }
    }, {
       key: "updateCursorXYPosition",
       value: function updateCursorXYPosition() {
          var cursor = this.fake.querySelector('cursor');
-         var cursorBox = cursor.getBoundingClientRect();
-         // var fakeBox = this.fake.getBoundingClientRect()
-         // var inputBox = this.input.getBoundingClientRect()
-
-         this.data.x = cursor.offsetLeft + this.input.offsetLeft; // - this.input.scrollLeft
+         this.data.x = cursor.offsetLeft + this.input.offsetLeft - this.input.scrollLeft;
          this.data.y = cursor.offsetTop + this.input.offsetTop - this.input.scrollTop;
       }
    }, {
       key: "updateFake",
       value: function updateFake() {
+         console.log('wtf');
          this.fake.style.cssText = document.defaultView.getComputedStyle(this.input, "").cssText;
-         //this.fake.style.visibility = "hidden"
-         var text = this.input.value.split('');
-         text.splice(this.data.position, 0, '<cursor style="display:inline-block">|</cursor>');
-         this.fake.innerHTML = text.join('');
-         document.body.appendChild(this.fake);
-
-         this.fake.scrollTop = this.input.scrollTop;
          this.fake.style.left = this.input.offsetLeft + 'px';
          this.fake.style.top = this.input.offsetTop + 'px';
          this.fake.style.pointerEvents = "none";
          this.fake.style.visibility = "hidden";
          this.fake.style.position = "fixed";
+
+         var text = this.input.value.split('');
+         text.splice(this.data.position, 0, '<cursor style="display:inline-block">|</cursor>');
+         this.fake.innerHTML = text.join('');
+         document.body.appendChild(this.fake);
       }
    }]);
 
@@ -142,8 +99,7 @@ var textarea = document.querySelector('textarea');
 var dataarea = document.querySelector('pre');
 var crosshair = document.querySelector('crosshair');
 
-var cursorWatch = new CursorWatch(textarea);
-cursorWatch.on('change', function (data) {
-   dataarea.innerHTML = JSON.stringify(data, null, '\t');
-   crosshair.style.transform = "translateX(" + data.x + "px) translateY(" + data.y + "px)";
+var cursorWatch = new CursorWatch(textarea, function () {
+   dataarea.innerHTML = JSON.stringify(this.data, null, '\t');
+   crosshair.style.transform = "translateX(" + this.data.x + "px) translateY(" + this.data.y + "px)";
 });
--- projects/Cursor Watch/src/js.js
+++ projects/Cursor Watch/src/js.js
@@ -1,71 +1,49 @@
 class CursorWatch {
-   constructor(input) {
+   constructor(input, callback) {
       this.input = input
+      this.callback = callback
       this.fake = document.createElement('div')
-      this.data = { position: 0, x: 0, y: 0 }
-      this.oldPosition = 0
+      this.data = { position: 0, lastPosition: -1, x: 0, y: 0 }
       this.listeners = []
       this.listen()
    }
 
-   on(type, callback) {
-      this.listeners.push({ type: type, callback: callback })
-   }
-
-   fire(type) {
-      for(var listener of this.listeners) {
-         if(listener.type == type) {
-            listener.callback(this.data)
-         }
-      }
-   }
-
    listen() {
-      this.interval = setInterval(() => {
-         this.updateCursorPosition()
-         if(this.oldPosition != this.data.position) {
-            this.change()
-         }
-      }, 1000/30)
-
-      this.input.addEventListener('scroll', () => { this.change() })
-   }
-
-   change() {
-      this.updateCursorPosition()
-      this.updateFake()
-      this.updateCursorXYPosition()
-      this.fire('change')
+      this.interval = setInterval(() => this.updateCursorPosition(), 1000/15)
+      this.input.addEventListener('scroll', () => this.change())
    }
 
    updateCursorPosition() {
       this.data.position = this.input.selectionStart
+      if(this.data.lastPosition != this.data.position) this.change()
+      this.data.lastPosition = this.data.position
+   }
+
+   change() {
+      this.updateFake()
+      this.updateCursorXYPosition()
+      this.callback()
    }
 
    updateCursorXYPosition() {
       var cursor = this.fake.querySelector('cursor')
-      var cursorBox = cursor.getBoundingClientRect()
-      // var fakeBox = this.fake.getBoundingClientRect()
-      // var inputBox = this.input.getBoundingClientRect()
-
-      this.data.x = cursor.offsetLeft + this.input.offsetLeft// - this.input.scrollLeft
+      this.data.x = cursor.offsetLeft + this.input.offsetLeft - this.input.scrollLeft
       this.data.y = cursor.offsetTop + this.input.offsetTop - this.input.scrollTop
    }
 
    updateFake() {
+      console.log('wtf')
       this.fake.style.cssText = document.defaultView.getComputedStyle(this.input, "").cssText;
-      //this.fake.style.visibility = "hidden"
-      var text = this.input.value.split('')
-      text.splice(this.data.position, 0, '<cursor style="display:inline-block">|</cursor>')
-      this.fake.innerHTML = text.join('')
-      document.body.appendChild(this.fake)
-
-      this.fake.scrollTop = this.input.scrollTop
       this.fake.style.left = this.input.offsetLeft + 'px'
       this.fake.style.top = this.input.offsetTop + 'px'
       this.fake.style.pointerEvents = "none"
       this.fake.style.visibility = "hidden"
       this.fake.style.position = "fixed"
+
+      var text = this.input.value.split('')
+      text.splice(this.data.position, 0, '<cursor style="display:inline-block">|</cursor>')
+      this.fake.innerHTML = text.join('')
+      document.body.appendChild(this.fake)
    }
 }
 
@@ -74,8 +52,7 @@ var textarea = document.querySelector('textarea')
 var dataarea = document.querySelector('pre')
 var crosshair = document.querySelector('crosshair')
 
-var cursorWatch = new CursorWatch(textarea)
-cursorWatch.on('change', function(data) {
-   dataarea.innerHTML = JSON.stringify(data, null, '\t')
-   crosshair.style.transform = `translateX(${data.x}px) translateY(${data.y}px)`
+var cursorWatch = new CursorWatch(textarea, function() {
+   dataarea.innerHTML = JSON.stringify(this.data, null, '\t')
+   crosshair.style.transform = `translateX(${this.data.x}px) translateY(${this.data.y}px)`
 })
