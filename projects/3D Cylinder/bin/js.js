7e20fc578f55fae33699d8bf41c964d7274e2b8f
sean codes <sean_codes@outlook.com>
2018-02-21 01:43:32 -0600

    combine cylinder/csssettings/gridinput

--- projects/3D Cylinder/bin/js.js
+++ projects/3D Cylinder/bin/js.js
@@ -22,4 +22,24 @@ if (!window.frameElement) {
    xhttp.send();
 }
 
-console.log('demo');
+new CSSettings({
+   settings: [{
+      label: 'perspective',
+      style: 'perspective',
+      suffix: 'px',
+      input: { type: 'range', min: 0, max: 1000, value: 1000 },
+      target: document.querySelector('.shape')
+   }, {
+      label: 'rotate X/Y',
+      style: 'transform',
+      input: { type: 'grid', min: -100, max: 100, step: 10, valueX: 0, valueY: 0 },
+      target: document.querySelector('.cylinder'),
+      create: function create() {
+         new GridInput({ input: this.html.input });
+      },
+      value: function value() {
+         var value = JSON.parse(this.html.input.value);
+         return "rotateX(" + value.y + "deg) rotateY(" + -value.x + "deg)";
+      }
+   }]
+});
