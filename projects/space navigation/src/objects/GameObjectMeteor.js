176bac9d0534129cb26dfa82fd58683637e53267
Sean Codes <sean_codes@outlook.com>
2019-05-28 00:38:00 -0500

    space navigation: meteor generator

--- projects/space navigation/src/objects/GameObjectMeteor.js
+++ projects/space navigation/src/objects/GameObjectMeteor.js
@@ -2,14 +2,28 @@ function ObjectMeteor(options) {
    this.pos = options.pos
    this.radius = options.radius || 50
 
+   this.vertices = []
+   var pointCount = 10 + Math.ceil(Math.random() * 15)
+   var pointsAngle = Math.PI*2 / pointCount
+   var start = Math.PI*2 * Math.random()
+
+   for (var i = 0; i < pointCount; i++) {
+      var radius = this.radius + (Math.random() * 6 - 3)
+      this.vertices.push({
+         x: Math.cos(start + pointsAngle * i) * radius,
+         y: Math.sin(start + pointsAngle * i) * radius
+      })
+   }
+
    this.step = () => {
-      engine.draw.settings({ strokeStyle: 'red', lineWidth: 5 })
-      engine.draw.circle({
-         pos: this.pos,
-         radius: this.radius,
-         fill: true,
-         stroke: true,
-         set: { fillStyle: '#222' }
+      engine.draw.settings({
+         strokeStyle: '#aaa',
+         lineWidth: 5
+      })
+
+      engine.draw.shape({
+         points: this.vertices,
+         relative: this.pos
       })
    }
 }
