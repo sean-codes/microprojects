a2039325c8d347ce83c485f23f6d3c5a51745aa9
Sean Codes <sean_codes@outlook.com>
2019-08-10 12:36:12 -0500

    snapshotting: curr/last snapshot on gameobject

--- projects/snapshotting/src/js.js
+++ projects/snapshotting/src/js.js
@@ -3,10 +3,17 @@ const GAME_HEIGHT = 200
 
 class Client {
    constructor() {
-      this.interval = setInterval(this.step.bind(this), 1000/60)
+      this.loopInfo = {
+         speed: 1000/60,
+         deltaSpeed: 1000/60, // for matching a clients framerate
+         delta: 1,
+         frames: 0,
+         last: Date.now(),
+         interval: setInterval(this.loop.bind(this), 1000/30)
+      }
 
       // setup canvas
-      this.ctx = canvas.getContext('2d')
+      this.ctx = htmlCanvas.getContext('2d')
       this.ctx.canvas.width = GAME_WIDTH
       this.ctx.canvas.height = GAME_HEIGHT
 
@@ -15,15 +22,25 @@ class Client {
          player: class {
             constructor(game) {
                this.game = game
-               this.x = GAME_WIDTH/2
-               this.y = GAME_HEIGHT/2
                this.radius = 30
+
+               this.timeline = {
+                  currSnapshot: undefined,
+                  lastSnapshot: undefined,
+                  timeBetween: 0,
+                  timeLast: 0,
+               }
             }
 
             step() {
+               var percent = (Date.now() - this.timeline.timeLast) / this.timeline.timeBetween
+               var x = this.timeline.lastSnapshot.x + (this.timeline.currSnapshot.x - this.timeline.lastSnapshot.x) * percent
+               var y = this.timeline.lastSnapshot.y + (this.timeline.currSnapshot.y - this.timeline.lastSnapshot.y) * percent
+
+
                this.game.ctx.clearRect(0, 0, this.game.ctx.canvas.width, this.game.ctx.canvas.height)
                this.game.ctx.beginPath()
-               this.game.ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2)
+               this.game.ctx.arc(x, y, this.radius, 0, Math.PI*2)
                this.game.ctx.fill()
             }
          }
@@ -43,13 +60,22 @@ class Client {
                this.gameObjects.push(object)
             }
 
-            object.x = snap.x
-            object.y = snap.y
+            if (!object.timeline.lastSnapshot) {
+               object.timeline.lastSnapshot = snap
+               object.timeline.timeBetween = 0
+               object.timeLast = 0
+            } else {
+               object.timeline.lastSnapshot = object.timeline.currSnapshot
+               object.timeline.timeBetween = Date.now() - object.timeline.timeLast
+               object.timeline.timeLast = Date.now()
+            }
+
+            object.timeline.currSnapshot = snap
          }
       }
    }
 
-   step() {
+   loop() {
       // draw events
       for (var gameObject of this.gameObjects) {
          gameObject.step()
@@ -147,6 +173,11 @@ class Server {
    }
 }
 
+// initialize
+var server = new Server
+var client = new Client
+server.connect(client)
 
-var server = new Server()
-server.connect(new Client())
+htmlClientFps.innerHTML = Math.round(1000/client.loopInfo.speed)
+htmlServerFps.innerHTML = Math.round(1000/server.loopInfo.speed)
+htmlServerSnapshotRate.innerHTML = Math.round(1000/server.snapshotInterval)
