1c9d9e0e73c8909309d6705cd6cdb72522d9c9c9
Sean Codes <sean_codes@outlook.com>
2020-01-18 19:49:46 -0600

    site-template: use real projects

--- projects/site-template/bin/js.js
+++ projects/site-template/bin/js.js
@@ -1,107 +1,134 @@
-// grab all the projects
-const exampleProject = {
-   title: '41 Mega pixel ',
-   subtitle: 'An example project card',
-   builtWith: ['js', 'scss', 'pug'],
-   url: '/microprojects/projects/41 Mega Pixel/index.html'
-}
-const projects = []
-
-for (var i = 0; i < 15; i++) {
-   projects[i] = { ...exampleProject }
-   projects[i].title += i
-}
-
 $(() => {
    $('#site').css({ opacity: 1 })
 
+   // globals
+   const $projectsJson = $('#projects-json')
+   const $pagination = $('#pagination')
+   const $buttonPrev = $('#prev')
+   const $buttonNext = $('#next')
+   const $searchInput = $('#search-input')
+   const $searchButton = $('#search-button')
+   const $alertNoResults = $('#alert-noresults')
+
+   $buttonPrev.on('click', prevPage)
+   $buttonNext.on('click', nextPage)
+   $searchButton.on('click', search)
+   $searchInput.on('keyup', e => e.keyCode === 13 && search())
+
+   const projects = JSON.parse($projectsJson.html()).projectsList
+
+   let filteredProjects = projects.filter(p => {
+      return !p['hide-on-index']
+   })
    let page = 0
-   displayProjects(page)
 
-   $buttonPrev = $('#prev')
-   $buttonNext = $('#next')
+   displayProjects(page, filteredProjects)
 
-   $buttonPrev.on('click', () => {
+   // some handies
+   function nextPage() {
+      if (page === Math.ceil(filteredProjects.length / 6) - 1) return
+      page += 1
+      displayProjects(page, filteredProjects)
+   }
+
+   function prevPage() {
       if (page === 0) return
       page -= 1
-      displayProjects(page)
-   })
-
-   $buttonNext.on('click', () => {
-      if (page == Math.ceil(projects.length / 6) - 1) return
-      page += 1
-      displayProjects(page)
-   })
-
-})
-
-function displayProjects(page) {
-   // get each of the project containers
-   const $containers = $('.project-card')
-   $containers.each(containerID => {
-      project = projects[page*6 + containerID]
-      $container1 = $($containers[containerID])
-      displayProject(project, $container1)
-   })
-
-   renderPagination(page)
-}
-
-function displayProject(project, $container) {
-   $container.removeClass('d-none')
-   $containerIframe = $container.find('iframe')
-   $containerTitle = $container.find('h5')
-   $containerSubtitle = $container.find('p')
-   $containerBadges = $container.find('.badge')
-
-
-   // reset project
-   $containerTitle.html('')
-   $containerSubtitle.html('')
-   $containerBadges.each(badgeId => {
-      $($containerBadges[badgeId]).html('')
-   })
-
-   $container.addClass('loading')
-
-   if (!project) {
-      $container.addClass('d-none')
-      $containerIframe.attr('src', '')
-      return
+      displayProjects(page, filteredProjects)
    }
 
-   $containerIframe.attr('src', project.url)
+   function search() {
+      $alertNoResults.addClass('d-none')
+      $pagination.addClass('d-none')
 
-   $containerIframe.on('load', (e) => {
-      $containerTitle = $container.find('h5')
-      $containerSubtitle = $container.find('p')
-      $containerBadges = $container.find('.badge')
-      $container.removeClass('loading')
-      $containerTitle.html(project.title)
-      $containerSubtitle.html(project.subtitle)
-      $containerBadges.each(badgeId => {
-         $($containerBadges[badgeId]).html(project.builtWith[badgeId])
+      // why am i using jquery? could have.... e.target.value. dork :]
+      const value = $searchInput.val()
+      page = 0
+      filteredProjects = projects.filter(p => {
+         return p.title.includes(value)
       })
 
-      $(e.target).off('load')
-   })
-}
+      if (value === '') {
+         filteredProjects = filteredProjects.filter(p => {
+            return !p['hide-on-index']
+         })
+      }
 
-function renderPagination(page) {
-   const $paginationContainer = $('.pagination')
-   const $btnPrevious = $paginationContainer.find('#prev')
-   const $btnNext = $paginationContainer.find('#next')
+      displayProjects(page, filteredProjects)
 
-   const pagesCount = Math.ceil(projects.length / 6)
+      if (filteredProjects.length === 0) {
+         $alertNoResults.removeClass('d-none')
+      }
 
-   $btnNext.removeClass('disabled')
-   $btnPrevious.removeClass('disabled')
-
-   if (page === 0) {
-      $btnPrevious.addClass('disabled')
+      if (filteredProjects.length > 6) {
+         $pagination.removeClass('d-none')
+      }
    }
 
-   if (page == pagesCount - 1) {
-      $btnNext.addClass('disabled')
+   function displayProjects(page, projects) {
+      // get each of the project containers
+      const $containers = $('.project-card')
+      $containers.each(containerID => {
+         project = projects[page*6 + containerID]
+         $container1 = $($containers[containerID])
+         displayProject(project, $container1)
+      })
+
+      renderPagination(page, projects)
    }
-}
+
+   function displayProject(project, $container) {
+      $container.removeClass('d-none')
+      $containerIframe = $container.find('iframe')
+      $containerTitle = $container.find('h5')
+      $containerDescription = $container.find('p')
+      // $containerBadges = $container.find('.badge')
+
+
+      // reset project
+      $containerTitle.html('')
+      $containerDescription.html('')
+      // $containerBadges.each(badgeId => {
+      //    $($containerBadges[badgeId]).html('')
+      // })
+
+      $container.addClass('loading')
+ 
+      if (!project) {
+         $container.addClass('d-none')
+         $containerIframe.attr('src', '')
+         return
+      }
+
+      $containerIframe.attr('src', '/microprojects/' + project.path)
+
+      $containerIframe.on('load', (e) => {
+         $containerTitle = $container.find('h5')
+         $containerDescription = $container.find('p')
+         $containerBadges = $container.find('.badge')
+         $container.removeClass('loading')
+         $containerTitle.html(project.title)
+         $containerDescription.html(project.description || 'a microproject!')
+         // $containerBadges.each(badgeId => {
+         //    $($containerBadges[badgeId]).html(project.builtWith[badgeId])
+         // })
+
+         $(e.target).off('load')
+      })
+   }
+
+   function renderPagination(page, projects) {
+      const pagesCount = Math.ceil(projects.length / 6)
+
+      $buttonNext.removeClass('disabled')
+      $buttonPrev.removeClass('disabled')
+
+      if (page === 0) {
+         $buttonPrev.addClass('disabled')
+      }
+
+      if (page == pagesCount - 1) {
+         $buttonNext.addClass('disabled')
+      }
+   }
+})
