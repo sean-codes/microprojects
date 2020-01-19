4b6dc4dfeb950a93cd85b642e815f53f83124fcc
Sean Codes <sean_codes@outlook.com>
2020-01-18 18:03:30 -0600

    site-template: pagination

--- projects/site-template/bin/js.js
+++ projects/site-template/bin/js.js
@@ -1,71 +1,54 @@
 // grab all the projects
-const projects = [
-   {
-      title: '41 Mega pixel 1',
-      subtitle: 'An example project card',
-      builtWith: ['js', 'scss', 'pug'],
-      url: '/microprojects/projects/41 Mega Pixel/index.html'
-   },
-   {
-      title: '41 Mega pixel 2',
-      subtitle: 'An example project card',
-      builtWith: ['js', 'scss', 'pug'],
-      url: '/microprojects/projects/41 Mega Pixel/index.html'
-   },
-   {
-      title: '41 Mega pixel 3',
-      subtitle: 'An example project card',
-      builtWith: ['js', 'scss', 'pug'],
-      url: '/microprojects/projects/41 Mega Pixel/index.html'
-   },
-   {
-      title: '41 Mega pixel 4',
-      subtitle: 'An example project card',
-      builtWith: ['js', 'scss', 'pug'],
-      url: '/microprojects/projects/41 Mega Pixel/index.html'
-   },
-   {
-      title: '41 Mega pixel 5',
-      subtitle: 'An example project card',
-      builtWith: ['js', 'scss', 'pug'],
-      url: '/microprojects/projects/41 Mega Pixel/index.html'
-   },
-   {
-      title: '41 Mega pixel 6',
-      subtitle: 'An example project card',
-      builtWith: ['js', 'scss', 'pug'],
-      url: '/microprojects/projects/41 Mega Pixel/index.html'
-   }
-]
+const exampleProject = {
+   title: '41 Mega pixel ',
+   subtitle: 'An example project card',
+   builtWith: ['js', 'scss', 'pug'],
+   url: '/microprojects/projects/41 Mega Pixel/index.html'
+}
+const projects = []
+
+for (var i = 0; i < 15; i++) {
+   projects[i] = { ...exampleProject }
+   projects[i].title += i
+}
 
 $(() => {
    $('#site').css({ opacity: 1 })
-   displayProjects()
+
+   let page = 0
+   displayProjects(page)
 
    $buttonPrev = $('#prev')
    $buttonNext = $('#next')
 
    $buttonPrev.on('click', () => {
-      console.log('previous')
+      if (page === 0) return
+      page -= 1
+      displayProjects(page)
    })
 
    $buttonNext.on('click', () => {
-      console.log('next')
-      displayProjects()
+      if (page == Math.ceil(projects.length / 6) - 1) return
+      page += 1
+      displayProjects(page)
    })
+
 })
 
-function displayProjects(start, end) {
+function displayProjects(page) {
    // get each of the project containers
    const $containers = $('.project-card')
    $containers.each(containerID => {
-      project = projects[containerID]
+      project = projects[page*6 + containerID]
       $container1 = $($containers[containerID])
       displayProject(project, $container1)
    })
+
+   renderPagination(page)
 }
 
 function displayProject(project, $container) {
+   $container.removeClass('d-none')
    $containerIframe = $container.find('iframe')
    $containerTitle = $container.find('h5')
    $containerSubtitle = $container.find('p')
@@ -80,9 +63,16 @@ function displayProject(project, $container) {
    })
 
    $container.addClass('loading')
+
+   if (!project) {
+      $container.addClass('d-none')
+      $containerIframe.attr('src', '')
+      return
+   }
+
    $containerIframe.attr('src', project.url)
 
-   $containerIframe.on('load', () => {
+   $containerIframe.on('load', (e) => {
       $containerTitle = $container.find('h5')
       $containerSubtitle = $container.find('p')
       $containerBadges = $container.find('.badge')
@@ -92,5 +82,26 @@ function displayProject(project, $container) {
       $containerBadges.each(badgeId => {
          $($containerBadges[badgeId]).html(project.builtWith[badgeId])
       })
+
+      $(e.target).off('load')
    })
+}
+
+function renderPagination(page) {
+   const $paginationContainer = $('.pagination')
+   const $btnPrevious = $paginationContainer.find('#prev')
+   const $btnNext = $paginationContainer.find('#next')
+
+   const pagesCount = Math.ceil(projects.length / 6)
+
+   $btnNext.removeClass('disabled')
+   $btnPrevious.removeClass('disabled')
+
+   if (page === 0) {
+      $btnPrevious.addClass('disabled')
+   }
+
+   if (page == pagesCount - 1) {
+      $btnNext.addClass('disabled')
+   }
 }
